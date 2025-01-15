980d31a46e63053b7e191cf867b7755692eb0120
Thenameisom31 <omkarpatil3516@gmail.com>
2025-01-15 11:24:11 +0530

    add new line in java file

--- JavaProgram.java
+++ JavaProgram.java
@@ -3,5 +3,6 @@ class javaProgram
 {
     public static void main(String[] args) {
         System.out.println("Hello world.... ");
+        System.out.println("\n this is from omkar ......");
     }
 }
