6fd99441fdec7b91079fe55d21e55e9b082e107f
Thenameisom31 <omkarpatil3516@gmail.com>
2025-01-15 11:50:30 +0530

    add print line

--- JavaProgram.java
+++ JavaProgram.java
@@ -4,5 +4,6 @@ class javaProgram
     public static void main(String[] args) {
         System.out.println("Hello world.... ");
         System.out.println("\n this is from omkar ......");
+        System.out.println("hello world");
     }
 }
