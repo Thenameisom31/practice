488c1b69958f881242039c9cf6750039cf3c9838
Thenameisom31 <omkarpatil3516@gmail.com>
2025-01-15 11:49:26 +0530

    add some value

--- JavaProgram.java
+++ JavaProgram.java
@@ -4,5 +4,6 @@ class javaProgram
     public static void main(String[] args) {
         System.out.println("Hello world.... ");
         System.out.println("\n this is from omkar ......");
+        int a =5; System.out.println("\n A value is :"+a);
     }
 }
