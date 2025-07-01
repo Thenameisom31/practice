576427ded074d69dbd8a4aa389af61ab74097ea5
Thenameisom31 <omkarpatil3516@gmail.com>
2025-07-01 12:11:05 +0530

    Check if two strings are rotations of each other.

--- String_Problem/CheckRotation.java
+++ String_Problem/CheckRotation.java
@@ -1,3 +1,22 @@
+/* Strings Rotations of Each Other
+Difficulty: EasyAccuracy: 43.83%Submissions: 286K+Points: 2Average Time: 15m
+You are given two strings of equal lengths, s1 and s2. The task is to check if s2 is a rotated version of the string s1.
+
+Note: The characters in the strings are in lowercase.
+
+Examples :
+
+Input: s1 = "abcd", s2 = "cdab"
+Output: true
+Explanation: After 2 right rotations, s1 will become equal to s2.
+Input: s1 = "aab", s2 = "aba"
+Output: true
+Explanation: After 1 left rotation, s1 will become equal to s2.
+Input: s1 = "abcd", s2 = "acbd"
+Output: false
+Explanation: Strings are not rotations of each other. */
+
+
 class CheckRotation
 {
     static public int[] calculateLps(String pat)
