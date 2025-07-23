2d87b5ddccfc6b82dc915d14bf56c577a2bd2bf7
Thenameisom31 <omkarpatil3516@gmail.com>
2025-07-23 11:08:02 +0530

    add method to print matrix elements in spiral order.

---
+++ Matrix_Problem/SpiralMatrixPrinter.java
@@ -0,0 +1,66 @@
+/* Print a given matrix in spiral form
+
+comments
+Given a matrix mat[][] of size m x n, the task is to print all elements of the matrix in spiral form.
+
+Examples: 
+
+Input: mat[][] = [[1,   2,   3,   4],
+                           [5,    6,   7,   8],
+                        [9,   10,  11,  12],
+                      [13,  14,  15,  16]]
+Output: [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10] */
+package Matrix_Problem;
+import java.util.ArrayList;
+public class SpiralMatrixPrinter {
+
+    public static ArrayList<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> result = new ArrayList<>();
+       
+
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+            // Traverse from left to right
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+
+            // Traverse from top to bottom
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                // Traverse from right to left
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                // Traverse from bottom to top
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+        System.out.println(spiralOrder(matrix)); 
+    }
+}
