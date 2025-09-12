9a80603db70792f9946ac14c260882b31f5bed71
Thenameisom31 <omkarpatil3516@gmail.com>
2025-09-12 23:49:10 +0530

    Add longest subarray with sum K.

---
+++ prefixSum_Problem/LongestSubarraySumK.java
@@ -0,0 +1,47 @@
+/* Longest Subarray with Sum K
+Difficulty: MediumAccuracy: 24.64%Submissions: 724K+Points: 4
+Given an array arr[] containing integers and an integer k, your task is to find the length of the longest subarray where the sum of its elements is equal to the given value k. If there is no subarray with sum equal to k, return 0.
+
+Examples:
+
+Input: arr[] = [10, 5, 2, 7, 1, -10], k = 15
+Output: 6
+Explanation: Subarrays with sum = 15 are [5, 2, 7, 1], [10, 5] and [10, 5, 2, 7, 1, -10]. The length of the longest subarray with a sum of 15 is 6.
+Input: arr[] = [-5, 8, -14, 2, 4, 12], k = -5
+Output: 5
+Explanation: Only subarray with sum = -5 is [-5, 8, -14, 2, 4] of length 5.
+Input: arr[] = [10, -10, 20, 30], k = 5
+Output: 0
+Explanation: No subarray with sum = 5 is present in arr[]. */
+
+package prefixSum_Problem;
+import java.util.HashMap;
+public class LongestSubarraySumK {
+
+    static int longestSubarrayWithSumK(int arr[], int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+        int prefSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            prefSum += arr[i];
+            if (prefSum == k) {
+                res = i + 1;
+            }
+            if (map.containsKey(prefSum - k)) {
+                res = Math.max(res, i - map.get(prefSum - k));
+            }
+            if (!map.containsKey(prefSum)) {
+                map.put(prefSum, i);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        
+        int arr[] = { 10, 5, 2, 7, 1, -10 };
+        int k = 15;
+        int result = longestSubarrayWithSumK(arr, k);
+        System.out.println("Length of the longest subarray with sum " + k + " is: " + result);
+    }
+}
